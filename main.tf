fb10531ed5b3678a5eea95258f17efa5b2c448fc
jorgeruiz27 <jorge.cx.27@gamil.com>
2024-07-18 08:48:41 -0400

    comando no shel automatizados

--- main.tf
+++ main.tf
@@ -1,6 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
 provider "aws" {
   region = "us-east-1"
 }
+
+/*
+terraform {
+  required_providers {
+    serverscom = {
+      source = "serverscom/serverscom"
+      version = "0.4.2"
+    }
+  }
+}
+
+provider "serverscom" {
+  # Configuration options
+}
+*/
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -18,11 +43,19 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "ec2" {
+resource "aws_instance" "app_server" {
   ami = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name = "jorge-ruizPuTTY"
+  user_data = <<-EOF
+                 #!/bin/bash
+                    cd /home/ubuntu
+                    echo "<h1>SITE COM TERRAFORM</h1>" > index.html
+                    echo "<h1>SITE COM TERRAFORM</h1>" > index1.html
+                    echo "<h1>SITE COM TERRAFORM</h1>" > index2.html
+                    nohup busybox httpd -f -p 8080 &
+                 EOF
   tags = {
-    Name = "PrimeiraInstancia"
+    Name = "Terrafor AWS"
   }
 }
